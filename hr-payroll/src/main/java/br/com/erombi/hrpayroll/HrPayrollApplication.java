93c93cd880664eb48766fd2b4347bec8fe0dfeb5
Eduardo Rombi <eduardo.rombi99@gmail.com>
2021-06-23 18:48:04 -0300

    Ribbon load balancing

--- hr-payroll/src/main/java/br/com/erombi/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/br/com/erombi/hrpayroll/HrPayrollApplication.java
@@ -2,7 +2,11 @@ package br.com.erombi.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
+@RibbonClient(name = "hr-worker")
 @SpringBootApplication
 public class HrPayrollApplication {
 
--- hr-payroll/src/main/java/br/com/erombi/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/br/com/erombi/hrpayroll/services/PaymentService.java
@@ -1,13 +1,21 @@
 package br.com.erombi.hrpayroll.services;
 
 import br.com.erombi.hrpayroll.entities.Payment;
+import br.com.erombi.hrpayroll.entities.Worker;
+import br.com.erombi.hrpayroll.feignclients.WorkerFeignClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PaymentService {
 
+    @Autowired
+    private WorkerFeignClient workerFeignClient;
+
     public Payment getPayment(long workerId, int days) {
-        return new Payment("Bob", 200.0, days);
+        Worker worker = workerFeignClient.findById(workerId).getBody();
+
+        return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 
 }
